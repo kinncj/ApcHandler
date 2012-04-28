460e2c91b135d2a9cec6100559e88f03bde48984
kinncj <kinncj@gmail.com>
2012-04-28 15:34:26 -0300

    First

--- public/cli_usage.php
+++ public/cli_usage.php
@@ -11,5 +11,7 @@ $keyValue1 = new \stdClass();
 $keyValue1->val1 = "a";
 $keyValue1->val2 = "b";
 $key1->setName("TestKey1")->setValue($keyValue1);
+$apc->add($key1);
+var_dump(array("Cli"=>$apc->store()));
 var_dump(array("Cli"=>$key1));var_dump(array("Cli"=>$key1->getValue()));
 var_dump(array("Cli"=>$apc->getKey($key1)));
